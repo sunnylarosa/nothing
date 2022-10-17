357b608260d57117a6a13cab07253c5572577b9d
Sunny Larosa <sunnylarosa121@gmail.com>
2022-10-18 02:00:05 +0700

    Add .flaskenv file

---
+++ .flaskenv
@@ -0,0 +1,4 @@
+FLASK_APP = saski.py
+FLASK_DEBUG = 0
+FLASK_ENV = development
+FLASK_RUN_PORT = 5000
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,10 @@
-import imp
 from app.main import main
-from flask import render_template
+from flask import render_template, current_app
 
 @main.route("/")
 @main.route("/index")
 def index():
+    print()
+    print(current_app.config["ENV"])
+    print()
     return render_template('base.html')
